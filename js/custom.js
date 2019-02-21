36ec4c11f7e4fd78fa6c4728c6bab4848347d101
Ben Fordham <fordham13@hotmail.co.uk>
2019-02-21 17:25:18 +0000

    project complete

--- js/custom.js
+++ js/custom.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
   $("#menu-btn").click(function(){
     $("#nav").slideToggle(500);
   });
+  $("#close-menu").click(function(){
+    $("#nav").slideToggle(500);
+  });
 });
